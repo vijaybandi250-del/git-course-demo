9f6fa41a26f87bd887248cca9a27cdf76da76d19
vijay <vijaybandi250@gmail.com>
2026-01-16 16:48:54 +0530

    changes has been done

--- src/main/java/com/experimentjava/firstproject/ControllerClass.java
+++ src/main/java/com/experimentjava/firstproject/ControllerClass.java
@@ -13,3 +13,17 @@ public class ControllerClass {
     }
 
 }
+
+class conditionals {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
