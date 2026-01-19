ac1d88281503f19f30559098d870b9735699e63a
vijay <vijaybandi250@gmail.com>
2026-01-19 17:28:20 +0530

    added

--- src/main/java/com/experimentjava/firstproject/Credentials.java
+++ src/main/java/com/experimentjava/firstproject/Credentials.java
@@ -17,4 +17,23 @@ public class Credentials {
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
     }
+
+    final class User {
+        private String username;
+        private String password;
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+        User user = new User("exampleUser", "examplePass");
+    }
 }
